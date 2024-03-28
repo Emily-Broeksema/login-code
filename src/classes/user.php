6ea244cb6c4a19c35ed32520163df9ed7d38b8f8
Emily-Broeksema <9017781@student.zadkine.nl>
2024-03-28 10:15:58 +0100

    forgotten commit

--- src/classes/user.php
+++ src/classes/user.php
@@ -84,9 +84,9 @@
             $dbname = "login";
 
             try {
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $this->$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
                 // set the PDO error mode to exception
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 //echo "Connected successfully<br>"; 
                 }
             catch(PDOException $e)
--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -1,48 +1,70 @@
 <?php
 
-use \PHPUnit\Framework\TestCase;
-use LoginOpdracht\classes\User;
+    use \PHPUnit\Framework\TestCase;
+    use LoginOpdracht\classes\User;
 
-class LoginTest extends TestCase 
-{
-    protected $user;
-    protected function setUp(): void
+    class LoginTest extends TestCase 
     {
-        $this->user = new User();
+        protected $user;
+        protected function setUp(): void
+        {
+            $this->user = new User();
+        }
+
+
+    public function testSetAndGetPassword()
+    {
+        $password = "bingbong!1";
+        $this->user->SetPassword($password);
+        $result = $this->user->GetPassword();
+        $this->assertEquals($password, $this->user->GetPassword());
     }
 
+    public function testValidateUserWithEmptyPassword()
+    {
+        $this->username = "feqEm";
+        $errors = $this->user->ValidateUser();
+        $this->assertContains("Username moet kleiner dan 3 en groter dan 50 tekens zijn.", $errors);
+    }
 
-public function testSetAndGetPassword()
-{
-    $password = "bingbong!1";
-    $this->user->SetPassword($password);
-    $this->assertEquals($password, $this->user->GetPassword());
-}
+    public function testRegisterUser()
+    {
+        $this->user->RegisterUser();
+        $isDeleted = (session_status() == PHP_SESSION_NONE || empty(session_id()));
+        $this->assertTrue(); // isDeleted
+    }
 
+    public function testValidateUserWithShortName()
+    {
+        $this->user->username = "ememe";
+        $errors = $this->user->ValidateUser();
+        $this->assertContains("Username moet > 3 en < 50 tekens zijn.", $errors);
+    }
 
+    public function testLogout()
+    {
+        session_start();
+        $this->user->Logout();
+        $isDeleted = (session_status() == PHP_SESSION_NONE || empty(session_id()));
+        $this->assertTrue($isDeleted);
+    }
 
-public function testValidateUserWithEmptyPassword()
-{
-    $this->username = "Emef2";
-    $errors = $this->user->ValidateUser();
-    $this->assertContains("Username moet kleiner dan 3 en groter dan 50 tekens zijn.", $errors);
-}
+    public function testIsLoggedInWhenNotLoggedIn()
+    {
+        $isLoggedIn = $this->user->IsLoggedIn();
+        $this->assertFalse($isLoggedIn, "User is considered logged in when not logged in");
+    }
 
-public function testValidateUserWithShortName()
-{
-    $this->user->username = "emenem";
-    $errors = $this->user->ValidateUser();
-    $this->assertContains("Username moet > 3 en < 50 tekens zijn.", $errors);
-}
+    public function testIsLoggedInWhenLoggedIn()
+    {
+        session_start();
+        $_SESSION['username'] = "test";
+        $isLoggedIn = $this->user->IsLoggedIn();
+        $this->assertTrue($isLoggedIn, "User is considered logged in when  logged in");
+    }
 
+    
+    
+    }
 
-
-public function testLogout()
-{
-    session_start();
-    $this->user->Logout();
-    $isDeleted = (session_status() == PHP_SESSION_NONE || empty(session_id()));
-    $this->assertTrue($isDeleted);
-}
-
-}
+?>
